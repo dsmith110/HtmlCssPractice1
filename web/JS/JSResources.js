588762b61598400e357133aade12e8031106579b
bit <bit@192.168.1.138>
2013-09-02 19:55:59 -0500

    I may add more but it fulfills all requirements

---
+++ web/JS/JSResources.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function showmenu(elmnt)
+{
+    document.getElementById(elmnt).style.visibility = "visible";
+}
+function hidemenu(elmnt)
+{
+    document.getElementById(elmnt).style.visibility = "hidden";
+}
+
+function storedInfo() 
+{
+    alert("Your information has been updated");
+}
